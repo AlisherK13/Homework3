c59029d4b3637cd18ed00a10744f1ac8d623d1e2
Alisher <kusainov-alisher@mail.ru>
2022-09-03 18:43:42 +0600

    added Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Возведите число А в натуральную степень В используя цикл
+Console.WriteLine("Введите число А: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите натуральную степень В: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+double c = Math.Pow(a, b);
+while(b > 0)
+{
+    Console.WriteLine($"Число А в степени В = {c}");
+    break;
+}
+if(b < 0)
+{
+    Console.WriteLine("Ошибка. Введите натуральное число.");
+}
