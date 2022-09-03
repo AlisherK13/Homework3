72b435b56576f1d26c7a1d14d550ca6bf830f2af
Alisher <kusainov-alisher@mail.ru>
2022-09-03 19:36:04 +0600

    added Task 6

---
+++ Task6/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами и вывести на экран
+int[] array = {0, 1, 0, 1, 0, 1, 0, 1};
+int index = 0;
+while(index < array.Length)
+{
+    Console.Write($"{array[index]} ");
+    index++;
+}
+Console.WriteLine();
