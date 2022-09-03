eba92a2b7d63bccf2bc48245aadc43fd850800f0
Alisher <kusainov-alisher@mail.ru>
2022-09-03 19:33:52 +0600

    added Task 5

---
+++ Task5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать кубы чисел заканчивающиеся на четную цифру
+Console.WriteLine("Введите ваше число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int b = a * a * a;
+int c = a % 2;
+if(c == 0)
+{
+    Console.WriteLine($"Куб числа = {b}");
+}
+else
+{
+    Console.WriteLine("Число не заканчивается на четную цифру");
+}
