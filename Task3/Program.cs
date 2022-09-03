0c99453fbfc821fe3091b769e3aa54099a1ad805
Alisher <kusainov-alisher@mail.ru>
2022-09-03 19:03:07 +0600

    added Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Введите ваше число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int b = 0;
+while(a > 0)
+{
+    b = b + a % 10;
+    a = a / 10;
+}
+Console.WriteLine($"Сумма цифр в вашем числе = {b}");
