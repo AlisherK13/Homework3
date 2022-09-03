cbaf27b49d78ebc0c310e84ac8d8c9b3a17d0f9f
Alisher <kusainov-alisher@mail.ru>
2022-09-03 19:14:09 +0600

    added Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введите число N: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+
+while (a < 0)
+{
+    Console.WriteLine("Некорректное число, введите натуральное число");
+    a = int.Parse(Console.ReadLine() ?? "0");
+}
+
+int b = 1;
+int sum = 1;
+
+while (b <= a)
+{
+    sum *= b;
+    b *= 1;
+    b++;
+}
+Console.WriteLine($"Произведение чисел от 1 до N = {sum}");
